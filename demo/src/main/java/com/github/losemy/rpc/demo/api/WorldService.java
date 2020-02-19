49205d7b5848795cad2fbebe8552d2ccecedbe1a
losemy <15927419634@163.com>
2020-02-19 19:53:38 +0800

    添加注释

--- demo/src/main/java/com/github/losemy/rpc/demo/api/WorldService.java
+++ demo/src/main/java/com/github/losemy/rpc/demo/api/WorldService.java
@@ -5,5 +5,10 @@ package com.github.losemy.rpc.demo.api;
  * @date 2019-11-11
  **/
 public interface WorldService {
+    /**
+     * 增加注释
+     * @param name
+     * @return
+     */
     String hello(String name);
 }
