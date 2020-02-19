b31fcda8dd75b3bbc5b3ad85391c9fad5203454a
losemy <15927419634@163.com>
2020-02-19 19:52:45 +0800

    添加注释

--- demo/src/main/java/com/github/losemy/rpc/demo/api/HelloService.java
+++ demo/src/main/java/com/github/losemy/rpc/demo/api/HelloService.java
@@ -2,6 +2,11 @@ package com.github.losemy.rpc.demo.api;
 
 public interface HelloService {
 
+    /**
+     * hello
+     * @param name
+     * @return
+     */
     String hello(String name);
 
     String hello(Person person);
